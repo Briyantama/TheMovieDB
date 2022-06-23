443d6fb31d3c31b3340072a058bbf26725cb4ab3
Briyan Wahab <briyan.whb@gmail.com>
2022-06-23 14:25:50 +0700

    update

--- app/src/main/java/com/example/challengechap7/utils/Event.kt
+++ app/src/main/java/com/example/challengechap7/utils/Event.kt
@@ -1,16 +1,10 @@
 package com.example.challengechap7.utils
 
-/**
- * Used as a wrapper for data that is exposed via a LiveData that represents an event.
- */
 open class Event<out T>(private val content: T) {
 
     var eventHandled = false
         private set
 
-    /**
-     * Returns the content and prevents its use again.
-     */
     fun getContentIfNotHandled(): T? {
         return if (eventHandled) {
             null
